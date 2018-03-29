52a5d81282d467002377eff69e955fd5c96540fc
Anton Panchishko <PanchishkoMayor@gmail.com>
2018-03-29 14:34:37 +0300

    git commit!

---
+++ src/tesapp/TesApp.java
@@ -0,0 +1,12 @@
+
+package tesapp;
+
+public class TesApp {/**
+* @param args the command line arguments
+*/
+public static void main(String[] args) {
+
+// TODO code application logic here
+System.out.println("GIT test application");
+}
+}
