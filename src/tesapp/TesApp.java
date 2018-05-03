261e73beac81ad5fb0aee471936469583d1dba3c
Anton Panchishko <PanchishkoMayor@gmail.com>
2018-05-03 17:10:20 +0300

    calcImproved

--- src/tesapp/TesApp.java
+++ src/tesapp/TesApp.java
@@ -1,18 +1,13 @@
 package tesapp;
-public class SimpleCalc {
+public class TesApp {
 
-private double a;
-private double b;
-
-SimpleCalc(double p1,double p2) {
-this.a=p1;
-this.b=p2;
+public static void main(String[] args) {
+Scanner in = new Scanner(System.in);
+System.out.print("1st num: ");
+double a = Double.valueOf(in.nextLine());
+System.out.print("2nd num: ");
+double b = Double.valueOf(in.nextLine());
+System.out.println("a+b="+(a+b));
+System.out.println("GIT test application");
 }
-
-public double getSum() {
-
-
-return this.a+this.b;
-}
-
 }
