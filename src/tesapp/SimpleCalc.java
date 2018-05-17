1de9898ae32dad60728e0096b51de7655d57aafd
Anton Panchishko <PanchishkoMayor@gmail.com>
2018-05-17 20:17:09 +0300

    Laba 5.2

--- src/tesapp/SimpleCalc.java
+++ src/tesapp/SimpleCalc.java
@@ -1,15 +1,27 @@
 package tesapp;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Anton
- */
 public class SimpleCalc {
-    
+
+private double a;
+private double b;
+private double c;
+SimpleCalc(double p1,double p2) {
+this.a=p1;
+this.b=p2;
+}
+public double getSum() {
+return this.a+this.b;
+}
+public double getDiff(){
+    return this.a-this.b;
+}
+public double getMulti(){
+     return this.a*this.b;
+ }
+public double getDiv(){
+     return this.a/this.b;
+ }
+
+
+
 }
